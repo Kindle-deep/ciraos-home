b790a239d38696a9cea9120e13f487b73bdba05d
ciraos <frank.ka@foxmail.com>
2023-01-07 18:34:35 +0800

    updated via upload

--- cw.js
+++ cw.js
@@ -2,4 +2,6 @@
  * cw.js
  */
 
-importScripts('https://lib.baomitu.com/clientworker/3.0.0-beta-3/dist/cw.js')
+// importScripts('https://lib.baomitu.com/clientworker/3.0.0-beta-3/dist/cw.js')
+// importScripts('https://cdn.jsdelivr.net/npm/clientworker@3.0.0-beta-4')
+importScripts("https://cdn.jsdelivr.net/npm/clientworker@3.0.0-beta-4/dist/cw.js")
